6137333981db73330815a6600db7dacd437a3ccb
bagasadit123 <bagasadit430@gmail.com>
2026-07-27 10:46:16 +0700

    validasi keberadaan user saat login

--- controller/userController.js
+++ controller/userController.js
@@ -53,3 +53,48 @@ async function login(req, res) {
                 message: 'Email dan password wajib di isi' 
             });
         }
+        
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        if (!user) {
+            return res.status(400).json({ 
+                message: 'Email atau password salah' 
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ 
+                message: 'Email atau password salah' 
+            });
+        }
+
+        const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN 
+        }
+    );
+
+        res.status(200).json({
+            message: 'Login berhasil',
+            token
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message 
+        });
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
