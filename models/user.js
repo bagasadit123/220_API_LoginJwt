2a6d0852ad95c905e52581f565e75414e97cd113
bagasadit123 <bagasadit430@gmail.com>
2026-07-27 09:59:35 +0700

    password

--- models/user.js
+++ models/user.js
@@ -9,3 +9,12 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             unique: true,
             allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    return User;
+};
