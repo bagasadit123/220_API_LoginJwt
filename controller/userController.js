b6b1dfe54412a91d943e49c46bf5dfca9456187b
bagasadit123 <bagasadit430@gmail.com>
2026-07-27 10:44:08 +0700

    implementasi fungsi register dengan hashing password

--- controller/userController.js
+++ controller/userController.js
@@ -13,7 +13,7 @@ async function register(req, res) {
                 message: 'Email and password wajib di isi' 
             });
         }
-        
+
         const existingUser = await User.findOne({
             where: { email }
         });
@@ -23,3 +23,78 @@ async function register(req, res) {
                 message: 'Email sudah terdaftar' 
             });
         }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const User = await User.create({
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: 'Registrasi berhasil',
+            user: {
+                id: User.id,
+                email: User.email
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message 
+        });
+    }
+}
+
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ 
+                message: 'Email dan password wajib di isi' 
+            });
+        }
+
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        if (!user) {
+            return res.status(400).json({ 
+                message: 'Email atau password salah' 
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ 
+                message: 'Email atau password salah' 
+            });
+        }
+
+        const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN 
+        }
+    );
+
+        res.status(200).json({
+            message: 'Login berhasil',
+            token
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message 
+        });
+    }
+}
+
+module.exports = {
+    register,
+    login
+};
